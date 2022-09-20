4dfad010276d0c9be82d56c76f8bcda6bbfa6439
hnz <ifyloop@outlook.com>
2022-09-20 22:05:11 +0300

    First Day

---
+++ aliens_invasion/aliens_invasion.py
@@ -0,0 +1,40 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    '''Класс для управления ресурсами поведения игры.'''
+    def __init__(self):
+        '''Инициализирует игру и создает игровые ресурсы'''
+        pygame.init()
+        #Назначение цвета фона
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self.screen)
+
+    def run_game(self):
+        '''Запускает основной цикл игры'''
+        while True:
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        # При каждом проходе цикла перересовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    #Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ aliens_invasion/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    '''Класс для хранения всех настроек игры'''
+
+    def __init__(self):
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (28, 28, 28)
