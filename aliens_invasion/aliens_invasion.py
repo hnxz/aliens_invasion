28042db37cc30ed7a76739ec6b5020871c7eb9b8
hnz <ifyloop@outlook.com>
2022-10-01 17:56:53 +0300

    First Day

--- aliens_invasion/aliens_invasion.py
+++ aliens_invasion/aliens_invasion.py
@@ -26,6 +26,10 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #переместить корабль вправо.
+                    self.ship.rect.x += 1
 
     def _update_screen(self):
         # При каждом проходе цикла перересовывается экран.
