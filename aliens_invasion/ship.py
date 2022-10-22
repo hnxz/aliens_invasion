3c267705932c04a9912edd4ba33ea6cba0093b91
hnz <ifyloop@outlook.com>
2022-10-22 23:39:38 +0300

    First Day

--- aliens_invasion/ship.py
+++ aliens_invasion/ship.py
@@ -17,9 +17,10 @@ class Ship():
         self.moving_left = False
     def update(self):
         '''Обновляет позицию коробля с учетом флага'''
-        if self.moving_right:
+        #Обновляется атрибут x объекта ship, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         # Обновление атрибута rect на основании self.x
         self.rect.x = self.x
